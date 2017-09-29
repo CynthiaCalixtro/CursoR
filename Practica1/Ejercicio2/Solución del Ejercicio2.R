0a36f1b0b57050e927f4785ddd43dfc8e96dff48
CynthiaCalixtro <ccalixtroa@uni.pe>
2017-09-29 13:47:33 -0500

    Ejercicio2.R

---
+++ Practica1/Ejercicio2/Solución del Ejercicio2.R
@@ -0,0 +1,11 @@
+# Nombre: Cynthia Lizeth Calixtro Ames 20150456D
+# Respuesta2: El codigo muestra la creaciones de los vectores especificados.
+
+# El vector x1 que contiene los valores 1, -2, 3, -4, ..., 99, -100 
+x1 <- 1:100 * c(1,-1)
+
+# El vector x2 que contiene los primeros 100 valores del factorial
+x2 <- factorial(1:100)
+
+# El vector x3 que contiene las primeras 100 potencias de 2
+x3 <- (1:100)**2
